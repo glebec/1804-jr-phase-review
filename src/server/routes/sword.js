e3a80437f19dc1973e7a60609e358b01cd6fd526
Gabriel Lebec <glebec@gmail.com>
2018-05-22 17:41:03 -0400

    feat(routes/swords): include smith

--- src/server/routes/sword.js
+++ src/server/routes/sword.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const router = require('express').Router()
-const { Sword } = require('../../db')
+const { Sword, Smith } = require('../../db')
 
 router.get('/', async (req, res, next) => {
     try {
-        const swords = await Sword.findAll()
+        const swords = await Sword.findAll({
+            include: Smith,
+        })
         res.json(swords)
     } catch (err) {
         next(err)

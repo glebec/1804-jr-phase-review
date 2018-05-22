9964da2f55d96514ecde86f565706f4227f67113
Gabriel Lebec <glebec@gmail.com>
2018-05-22 18:16:42 -0400

    wip(client/swords): connect swords component to redux

--- src/client/index.js
+++ src/client/index.js
@@ -2,9 +2,16 @@
 
 import React from 'react'
 import reactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
 import Swords from './swords/swords.jsx'
+import store from './redux'
 
 // const HelloWorld = () => <h1>Hello World</h1>
 
-reactDOM.render(<Swords />, document.getElementById('app'))
+reactDOM.render(
+    <Provider store={store}>
+        <Swords />
+    </Provider>,
+    document.getElementById('app')
+)
--- src/client/swords/swords.jsx
+++ src/client/swords/swords.jsx
@@ -1,21 +1,35 @@
 'use strict'
 
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default class Swords extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            swords: [],
-        }
-    }
+export class Swords extends React.Component {
+    // constructor(props) {
+    //     super(props)
+    // }
 
     render() {
-        return (
-            <>
-                <h2>Swords</h2>
-                <p>swords will go here</p>
-            </>
-        )
+        const { status } = this.props.swords
+        switch (status) {
+            case 'unasked':
+                return <p>Nobody wants to see any swords, I guess.</p>
+            case 'loading':
+                return <p>Loading some swords!</p>
+            default:
+                return (
+                    <>
+                        <h2>Swords</h2>
+                        <p>swords will go here</p>
+                    </>
+                )
+        }
     }
 }
+
+const mapStateToProps = ({ swords }) => ({
+    swords,
+})
+
+const mapDispatchToProps = dispatch => ({})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Swords)
